11eba406b57763705e49c99c3e304e3fc5552f80
Tri Nguyen <mankinchi.1995@gmail.com>
2022-11-12 10:57:32 +1100

    add ts eslint prettier

---
+++ src/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['airbnb', 'airbnb/hooks', 'airbnb-typescript'],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: '/src/tsconfig.json',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-indent': ['error', 'tab'],
+		'@typescript-eslint/indent': ['error', 'tab'],
+	},
+};
